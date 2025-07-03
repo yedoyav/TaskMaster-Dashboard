de6d99867ede310d49aaac42d0b164d9780b1f10
Yedo Stuani <yedo@yavdigital.com.br>
2025-07-03 13:55:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,33 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  transpilePackages: ['genkit'],
+  experimental: {
+    allowedDevOrigins: ["6000-firebase-studio-1748517146798.cluster-kc2r6y3mtba5mswcmol45orivs.cloudworkstations.dev"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yavdigital.com.br',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
